9559a43303a8a952fa491bba1d4560183e854c75
dragod812 <dragod@DESKTOP-NMNBK2O.localdomain>
2020-08-31 00:15:12 +0530

    First code-gen and [go, grpc and protoc] setup

---
+++ setup-go-grpc.sh
@@ -0,0 +1,13 @@
+# install go
+wget https://golang.org/dl/go1.15.linux-amd64.tar.gz
+sudo tar xzvf go1.15.linux-amd64.tar.gz
+sudo mv go /usr/local
+export GOPATH=$HOME/go
+export PATH=$PATH:/usr/local/go/bin:$GOPATH/bin
+
+# make GOPATH and src
+mkdir go && mkdir go/src
+
+# install protobuf compiler and grpc 
+sudo apt install -y protobuf-compiler
+go get -u google.golang.org/grpc
